b4f6e22f84e33fe552dca226692853b731e9bb3a
Anton <gimon2000@yandex.ru>
2024-05-31 23:04:57 +0300

    sprint_14 - сверстал launch screen

---
+++ Tracker/Extensions/UIColor+Extension.swift
@@ -0,0 +1,12 @@
+//
+//  UIColor+Extension.swift
+//  Tracker
+//
+//  Created by gimon on 31.05.2024.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBlue: UIColor{ UIColor(named: "Blue") ?? .blue }
+}
